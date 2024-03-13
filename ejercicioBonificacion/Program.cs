864e88b25dc298513f4a4f9df6e21ae9f5036ca6
Frank <frank.progra@gmail.com>
2024-03-12 23:12:12 -0600

    Agregar archivos de proyecto.

---
+++ ejercicioBonificacion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioBonificacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese años Laborales: ");
+            double anios = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese salario: ");
+            double salario = double.Parse(Console.ReadLine());
+            double salarioTotal = 0;
+            if ( anios >=1 && anios < 2)
+            {
+                salarioTotal = salario + (salario *0.15);
+            }
+            else if ( anios >=2  && anios <= 5)
+            {
+                salarioTotal = salario + (salario *0.20);
+            }else if (anios > 5 ) 
+            {
+                salarioTotal = salario + (salario * 1.25);
+            }
+            Console.WriteLine("salario total es de : {0}", salarioTotal);
+
+        }
+    }
+}
